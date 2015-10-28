766a69a40493d3f6f923052f31527204918503ab
dktlh <724279138@qq.com>
2015-10-28 19:58:56 +0800

    hhhh

--- app/src/main/java/com/example/tao/customview_1/CustomView.java
+++ app/src/main/java/com/example/tao/customview_1/CustomView.java
@@ -11,6 +11,10 @@ import android.util.Log;
 import android.util.TypedValue;
 import android.view.View;
 
+import java.util.HashSet;
+import java.util.Random;
+import java.util.Set;
+
 /**
  * Created by tao on 2015/10/28.
  */
@@ -63,11 +67,69 @@ public class CustomView extends View {
         mBound = new Rect();
         mPaint.getTextBounds(mTitleText, 0, mTitleText.length(), mBound);
 
+        this.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mTitleText = randomText();
+                //重绘
+                postInvalidate();
+            }
+        });
+
     }
 
+    private String randomText() {
+        Random random = new Random();
+        Set<Integer> set = new HashSet<>();
+        while (set.size() < 4) {
+            int randomInt = random.nextInt(10);
+            set.add(randomInt);
+        }
+        //StringBuilder比StringBufferY要快
+        StringBuilder sb = new StringBuilder();
+        for (Integer i : set) {
+            sb.append("" + i);
+        }
+        return sb.toString();
+    }
+
+    /**
+     * MeasureSpec的specMode一共三种类型：
+     * EXACTLY：一般是设置了明确的值或者是MATCH_PARENT
+     * AT_MOST：表示子布局限制在一个最大值内，一般为WARP_CONTENT
+     * UNSPECIFIED：表示子布局想要多大就多大，很少使用
+     *
+     * @param widthMeasureSpec
+     * @param heightMeasureSpec
+     */
     @Override
     protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
-        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+        int widthMode = MeasureSpec.getMode(widthMeasureSpec);
+        int widthSize = MeasureSpec.getSize(widthMeasureSpec);
+        int heightMode = MeasureSpec.getMode(heightMeasureSpec);
+        int heightSize = MeasureSpec.getSize(heightMeasureSpec);
+        int width;
+        int height;
+        if (widthMode == MeasureSpec.EXACTLY) {
+            width = widthSize;
+        } else {
+            mPaint.setTextSize(mTitleTextSize);
+            mPaint.getTextBounds(mTitleText, 0, mTitleText.length(), mBound);
+            float textWidth = mBound.width();
+            int desired = (int) (getPaddingLeft() + textWidth + getPaddingRight());
+            width = desired;
+        }
+
+        if (heightMode == MeasureSpec.EXACTLY) {
+            height = heightSize;
+        } else {
+            mPaint.setTextSize(mTitleTextSize);
+            mPaint.getTextBounds(mTitleText, 0, mTitleText.length(), mBound);
+            float textHeight = mBound.height();
+            int desired = (int) (getPaddingTop() + textHeight + getPaddingBottom());
+            height = desired;
+        }
+        setMeasuredDimension(width, height);
     }
 
     @Override
